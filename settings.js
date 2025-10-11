ce31185bea8b236f4c488adef059b4879d1c6501
nodezyx <subzeronodes@gmail.com>
2025-10-11 11:06:54 +0200

    Update settings.js for subzero2

--- settings.js
+++ settings.js
@@ -1,8 +1,6 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
-  CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
+  SESSION_ID: "SUBZERO~d1b41e2e3fa5cbe99dc464bac43b1a224db47f93",
+  OWNER_NUMBER: "263", 
+  PREFIX: ".",
+  CDN: "https://mrfrankk-cdn.hf.space"
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
